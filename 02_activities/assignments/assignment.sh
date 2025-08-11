248c7f7a315ccfc0f28c54f39a3b417d961d9205
lianml2025 <isabelle.lian@alumni.utoronto.ca>
2025-08-10 22:26:16 -0400

    Update assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -51,8 +51,7 @@ ls
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 
 
-
-cp ../raw/*server*.log ../processed/server_logs/ 
+cp ../raw/rawdata/*server*.log ../processed/server_logs/ 
 
 
 # 6. Repeat the above step for user logs and event logs
