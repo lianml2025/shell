f883cbfd8c7d07b417b6db1c34f7e8d679a397ef
lianml2025 <isabelle.lian@alumni.utoronto.ca>
2025-08-11 20:29:24 -0400

    Update assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -44,7 +44,7 @@ mkdir ./data/processed/server_logs ./data/processed/user_logs ./data/processed/e
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 
-xcopy ./data/raw/rawdata/rawdata/*server*.log -Destination ./data/processed/server_logs/ -Verbose
+xcopy ./data/raw/*server*.log -Destination ./data/processed/server_logs/ -Verbose
 
 
 
